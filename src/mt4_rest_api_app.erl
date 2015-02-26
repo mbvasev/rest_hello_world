e688cf0a7d3b58ef9e014cb293bb8517b95c717a
Martin Vasev <mbvasev@gmail.com>
2015-02-26 17:55:57 +0200

    groups and enable to create_account

--- src/mt4_rest_api_app.erl
+++ src/mt4_rest_api_app.erl
@@ -11,6 +11,7 @@
 %% API.
 
 start(_Type, _Args) ->
+	ok = lager:start(),
 	ok = application:start(crypto),
 	ok = application:start(ranch),
   ok = application:start(cowlib),
--- src/toppage_handler.erl
+++ src/toppage_handler.erl
@@ -12,9 +12,8 @@
 -export([init/3, content_types_accepted/2, known_methods/2, allowed_methods/2, options/2, process_json_request/2, prepare_json_response/2]).
 %% -export([service_available/2]).
 -export([content_types_provided/2]).
-
+-compile([{parse_transform, lager_transform}]).
 init(_Transport, _Req, []) ->
-	io:format("init ~n"),
 	{upgrade, protocol, cowboy_rest}.
 
 %% service_available(Req, State)->
@@ -28,7 +27,6 @@ init(_Transport, _Req, []) ->
 
 
 options(Req, State)->
-	io:format("options ~n"),
  	{Origin, _} = cowboy_req:header(<<"origin">>,Req),
 	Req1 = cowboy_req:set_resp_header(<<"access-control-allow-methods">>, <<"GET,POST,OPTIONS">>, Req),
 	Req2 = cowboy_req:set_resp_header(<<"Access-Control-Allow-Origin">>, Origin, Req1),
@@ -37,8 +35,11 @@ options(Req, State)->
 	{ok, Req5, State}.
 
 known_methods(Req, State)->
-	{Method,_} = cowboy_req:method(Req),
-	io:format("known_methods ~p ~n",[Method]),
+%% 	{{Ip,_},_} = cowboy_req:peer(Req),
+%% 	lager:info("peer ~p ~n",[Ip]),
+%%
+%% 	{Method,_} = cowboy_req:method(Req),
+%% 	io:format("known_methods ~p ~n",[Method]),
 	{[<<"GET">>, <<"POST">>,<<"OPTIONS">>], Req, State}.
 
 %% uri_too_long(Req, State)->
@@ -52,7 +53,6 @@ known_methods(Req, State)->
 %% 	end.
 
 allowed_methods(Req, State)->
-	io:format("allowed_methods ~n"),
 %% 	{Path,_} = cowboy_req:path(Req),
 %% 	PathTokens = string:tokens(binary_to_list(Path),"/"),
 	Resource = get_quered_resource(Req),
@@ -74,7 +74,6 @@ content_types_provided(Req, State) ->
 get_quered_resource(Req)->
 	{Path,_} = cowboy_req:path(Req),
 	PathTokens = string:tokens(binary_to_list(Path),"/"),
-	io:format("get_quered_resource ~p ~n",[PathTokens]),
 	case PathTokens of
 		["restapi",?ACCOUNTS_RESOURCE]->
 			{?ACCOUNTS_RESOURCE};
@@ -87,7 +86,6 @@ get_quered_resource(Req)->
 	end.
 
 process_json_request(Req, State) ->
-	io:format("process_json_request ~n"),
 	{Origin, _} = cowboy_req:header(<<"origin">>,Req),
 	Reply = cowboy_req:set_resp_header(<<"Access-Control-Allow-Origin">>, Origin, Req),
 	Resource = get_quered_resource(Req),
@@ -97,38 +95,46 @@ process_json_request(Req, State) ->
 		{?ACCOUNTS_RESOURCE,AccountId,?BALANCEOPS_RESOURCE} ->
 			create_balance_op(AccountId,Reply, State);
 		_ ->
+			lager:error("unknown resource: ~p",[Resource]),
 			{ok, Reply} = cowboy_req:reply(404, [], [], Req),
 			{halt, Reply, State}
 	end.
 
 create_balance_op(AccountId, Req, State)->
-	io:format("create_balance_op enter~n"),
 	{ok, Body, _} = cowboy_req:body(Req),
 	{struct, JsonData} = mochijson2:decode(Body),
 	Amount = proplists:get_value(<<"amount">>, JsonData),
 	Comment = proplists:get_value(<<"comment">>, JsonData),
 	Amm =  bin_to_num(Amount),
 	Login = list_to_integer(AccountId),
-	ValResult =  validate_ballop_params(Login,Amm,Comment),
-	if
-		ValResult =:= false->
-		cowboy_req:reply(404, [], [], Req),
-		{halt, Req, State}
-	end,
+%% 	ValResult =  validate_ballop_params(Login,Amm,Comment),
+%% 	if
+%% 		ValResult =:= false->
+%% 		cowboy_req:reply(400, [], [], Req),
+%% 		{halt, Req, State}
+%% 	end,
 
-	io:format("going to call create_balance_operation ~p ~n",[JsonData]),
-	case mt4_direct_connection:create_balance_operation(Login,Amm,binary_to_list(Comment)) of
+	lager:info("going to call create_balance_operation Login: ~p ~p ~n",[Login,JsonData]),
+	case catch(mt4_direct_connection:create_balance_operation(Login,Amm,binary_to_list(Comment))) of
 		{ok,Ticket}->
+			lager:info("balop created ticket: ~p",[Ticket]),
  			ReplyBody = mochijson2:encode({struct, [{ticket, Ticket}]}),
  			{ok, _Reply} = cowboy_req:reply(201, [], ReplyBody, Req),
 			{true,Req,State};
-		{error,_}->
+		{'EXIT',Reason}->
+			lager:error("'EXIT': ~p",[Reason]),
 			{ok, Reply} = cowboy_req:reply(404, [], [], Req),
 			{halt, Reply, State};
-		{badarg,_}->
+		{error,ErrMessage}->
+			lager:error("error: ~p",[ErrMessage]),
+			{ok, Reply} = cowboy_req:reply(404, [], [], Req),
+			{halt, Reply, State};
+		{badarg,Arg}->
+			lager:error("badarg: ~p",[Arg]),
 			{ok, Reply} = cowboy_req:reply(404, [], [], Req),
 			{halt, Reply, State};
 		badarg->
+			lager:error("badarg:"),
 			{ok, Reply} = cowboy_req:reply(404, [], [], Req),
 			{halt, Reply, State}
 	end.
@@ -153,39 +159,59 @@ create_account(Req, State)->
 	Phone = proplists:get_value(<<"phone">>, JsonData),
 	Country = proplists:get_value(<<"country">>, JsonData),
 	Zip = proplists:get_value(<<"zip">>, JsonData),
-	Comment = "Created by REST API",
-	io:format("Going to call create account ~n"),
-	case mt4_direct_connection:create_account(binary_to_list(Name),binary_to_list(Address),binary_to_list(Email),binary_to_list(City),Comment,binary_to_list(Id),binary_to_list(Phone),binary_to_list(Country),binary_to_list(Zip),"GMART-USD2P",100) of
+	Group = proplists:get_value(<<"group">>, JsonData),
+	EnableAccount = proplists:get_value(<<"enabled">>, JsonData),
+	Leverage = proplists:get_value(<<"leverage">>, JsonData),
+	Comment = "dd",
+	lager:info("Going to call create account with params ~p",[JsonData]),
+	AcountInfo = #mt4_user{name = binary_to_list(Name),address =  binary_to_list(Address),email =binary_to_list(Email), city = binary_to_list(City),comment =  Comment,id =binary_to_list(Id),
+		phone = binary_to_list(Phone),country =  binary_to_list(Country),zipcode =  binary_to_list(Zip),group =  binary_to_list(Group),leverage =  binary_to_integer(Leverage),enable = EnableAccount},
+
+	case catch(mt4_direct_connection:create_account(AcountInfo)) of
 		{ok,Login} ->
+			lager:info("account create login is ~p",[Login]),
 			ReplyBody = mochijson2:encode({struct, [{login, Login}]}),
 			{ok, _Reply} = cowboy_req:reply(201, [], ReplyBody, Req),
 			{true, Req, State};
-		{error,_}->
-			{ok, Reply} = cowboy_req:reply(204, [], [], Req),
+		{error,ErrMsg}->
+			lager:error("error ~p",[ErrMsg]),
+			{ok, Reply} = cowboy_req:reply(204, [], mochijson2:encode({struct, [{error, list_to_binary(ErrMsg)}]}), Req),
 			{halt, Reply, State};
-		{badarg,_}->
-			{ok, Reply} = cowboy_req:reply(404, [], [], Req),
+		{'EXIT',Reason}->
+			lager:error("EXIT ~p",[Reason]),
+			ReplyBody = mochijson2:encode({struct, [{error, Reason}]}),
+			{ok, Reply} = cowboy_req:reply(404, [], ReplyBody, Req),
+			{halt, Reply, State};
+		{badarg,Arg}->
+			lager:error("badarg ~p",[Arg]),
+			{ok, Reply} = cowboy_req:reply(404, [],mochijson2:encode({struct, [{error, <<"badarg_">>}]}),Req),
 			{halt, Reply, State};
 		badarg->
-			{ok, Reply} = cowboy_req:reply(404, [], [], Req),
+			lager:error("badarg"),
+			{ok, Reply} = cowboy_req:reply(404, [],mochijson2:encode({struct, [{error, <<"badarg">>}]}),Req),
 			{halt, Reply, State}
 	end.
 
 
 
 prepare_json_response(Req, State) ->
-	io:format("get_json ~n"),
 	{Origin, _} = cowboy_req:header(<<"origin">>,Req),
 	Req1 = cowboy_req:set_resp_header(<<"Access-Control-Allow-Origin">>, Origin, Req),
 	{Path,_} = cowboy_req:path(Req),
 	PathTokens = string:tokens(binary_to_list(Path),"/"),
 	LastToken = lists:last(PathTokens),
 	Account =list_to_integer(LastToken),
-	case mt4_direct_connection:get_account(Account) of
+	case catch(mt4_direct_connection:get_account(Account)) of
+		{'EXIT',Reason}->
+			lager:error("EXIT : ~p",[Reason]),
+			{ok, Reply} = cowboy_req:reply(404, [], [], Req1),
+			{halt, Reply, State};
 		not_found->
+			lager:warning("account ~p not found",[Account]),
 			{ok, Reply} = cowboy_req:reply(404, [], [], Req1),
 			{halt, Reply, State};
 		{ok,UserInfo} ->
+			lager:info("account ~p found",[Account]),
 			Answer = {struct, [{balance, UserInfo#mt4_user.balance}]},
 			RespBody = mochijson2:encode(Answer),
 			{RespBody, Req1, State}
